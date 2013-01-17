eba60e63c855b279840fdc6496ed5a85407e0b0e
Micah <micahcomer@gmail.com>
2013-01-17 08:48:13 -0600

    Initial commit.

--- TheMaze-OpenGLVersion/src/com/mjc/themaze_openglversion/GameApplication.java
+++ TheMaze-OpenGLVersion/src/com/mjc/themaze_openglversion/GameApplication.java
@@ -1,42 +1,58 @@
 package com.mjc.themaze_openglversion;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Screen;
+import com.mjc.themaze_openglversion.screens.GameScreen;
+import com.mjc.themaze_openglversion.screens.LoadScreen;
+import com.mjc.themaze_openglversion.screens.PauseScreen;
+import com.mjc.themaze_openglversion.screens.StartScreen;
 
 public class GameApplication implements ApplicationListener{
 
+	private static StartScreen startScreen;
+	private static LoadScreen loadScreen;
+	private static GameScreen gameScreen;
+	private static PauseScreen pauseScreen;
+	private Screen currentScreen;
+	
+	private long delta;
+	
 	@Override
 	public void create() {
-		// TODO Auto-generated method stub
-		
+		//Game Screen Manager initialization code should go here.
+		startScreen.show();
+		loadScreen.hide();
+		gameScreen.hide();
+		pauseScreen.hide();
+		currentScreen = startScreen;
 	}
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
+		//Code for final disposal of GSM should go here.
 		
 	}
 
 	@Override
 	public void pause() {
-		// TODO Auto-generated method stub
+		// Code to pause the game and save any needed information should go here.
 		
 	}
 
 	@Override
 	public void render() {
-		// TODO Auto-generated method stub
-		
+		currentScreen.render(delta);		
 	}
 
 	@Override
 	public void resize(int arg0, int arg1) {
-		// TODO Auto-generated method stub
+		// Code to deal with window resizing should go here.
 		
 	}
 
 	@Override
 	public void resume() {
-		// TODO Auto-generated method stub
+		// Unpausing code should go here.
 		
 	}
 
