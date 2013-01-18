125bec7137b6795fda01901302db2a326e8c6f80
Micah <micahcomer@gmail.com>
2013-01-18 17:29:21 -0600

    Start screen background works. New game button displays on screen, and responds to clicks, fires an empty method.

--- TheMaze-OpenGLVersion/src/com/mjc/themazeopenglversion/screens/StartScreen.java
+++ TheMaze-OpenGLVersion/src/com/mjc/themazeopenglversion/screens/StartScreen.java
@@ -1,17 +1,21 @@
 package com.mjc.themazeopenglversion.screens;
 
-
-
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
-public class StartScreen implements Screen{
+public class StartScreen implements Screen, EventListener{
 
 	Texture background;	
 	Vector2 screenDimensions; 
@@ -24,12 +28,43 @@ public class StartScreen implements Screen{
 	
 	SpriteBatch spriteBatch;
 	
+	private Stage stage;
+	
 	public StartScreen(AssetManager assetManager, SpriteBatch spriteBatch){
+		
 		screenDimensions = new Vector2 (Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		setStage();		
 		setBackground(assetManager);
 		this.spriteBatch = spriteBatch;
 		
 	}
+	
+	private void setStage(){
+		
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+	}
+	
+	private void setBackground(AssetManager assetManager){
+		
+		assetManager.load("startscreen_background.png", Texture.class);		
+		assetManager.load("button_start.png", Texture.class);			
+		assetManager.finishLoading();			
+		background = assetManager.get("startscreen_background.png", Texture.class);
+		Texture startButtonTexture = assetManager.get("button_start.png", Texture.class);
+		TextureRegion startButtonTR = new TextureRegion(startButtonTexture);
+		TextureRegionDrawable startButtonTRD = new TextureRegionDrawable(startButtonTR);
+		startButton = new Button(startButtonTRD);	
+		startButton.setBounds(0f, 0f, 400, 100);
+		startButton.setPosition(((float)screenDimensions.x)*(3.5f/5f), (float)(screenDimensions.y)*(3f/4f));
+		startButton.addListener(this);
+		stage.addActor(startButton);		
+		
+	}
+	
 	
 	@Override
 	public void dispose() {
@@ -56,12 +91,12 @@ public class StartScreen implements Screen{
 		
 		spriteBatch.begin();
 		//FIRST 2 float Coordinates are the BOTTOM-left corner of the screen!
-		//800 and 433 are the height of the actual bmp before I had to increase the size to powers of two.
-	
-		
-		spriteBatch.draw(background, 0f, 0f, screenDimensions.x, screenDimensions.y, 0,0,800,433,false, false);
+		//800 and 433 are the height of the actual bmp before I had to increase the size to powers of two.	
+		spriteBatch.draw(background, 0f, 0f, screenDimensions.x, screenDimensions.y, 0,0,800,433,false, false);		
 		spriteBatch.end();
 		
+		stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
 	}
 
 	@Override
@@ -83,13 +118,21 @@ public class StartScreen implements Screen{
 		
 	}
 	
-	private void setBackground(AssetManager assetManager){
+	@Override
+	public boolean handle(Event e) {
 		
-		assetManager.load("startscreen_background.png", Texture.class);		
-		while (!assetManager.update()){ }		
-		background = assetManager.get("startscreen_background.png", Texture.class);
-		
+		if (e.getListenerActor() == startButton){
+			startGame();
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	private void startGame(){
 		
 	}
+	
 
 }
